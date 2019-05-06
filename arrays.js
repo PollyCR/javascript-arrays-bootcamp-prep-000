c4c47a9abb9d6eaa7c08583deb4be5dc9902cd3b
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-06 14:18:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,6 +38,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length - 0)
+  array.slice(0, array.length - 2)
   return array
 }
