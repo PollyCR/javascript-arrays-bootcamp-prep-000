fcfe22ae3df4f9ba70d3735959efbb6ead04f1d8
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-06 14:13:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -37,6 +37,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 var items = [1, 2, 3, 4, 5]
-[...items.slice(0, 2)],
-[...items slice(3)]
+[...items.slice(0, 2)
+...items slice(3)]
 
